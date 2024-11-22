643020075affd3c5e563065412a73174c967d2e6
nilsong87 <nilsonjosesilvagomes@hotmail.com>
2024-11-22 02:52:21 -0300

    Primeiro commit

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,13 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/*.css',
-                    'resources/js/*.js',
-                    'resources/assets/images/*.jpg'
-                ],
-            refresh: true,
-        }),
-    ],
+  plugins: [
+    laravel({
+      input: ['resources/js/app.js', 'resources/css/app.css'],
+      refresh: true,
+    }),
+  ],
+  optimizeDeps: {
+    include: [], // Remova 'some-dependency' se não for necessária
+  },
 });
